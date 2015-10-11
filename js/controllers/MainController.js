ef79980e10dddb4a0c93ed7ff55f281709c0c54a
RiceeeChang <riceeechang@gmail.com>
2015-10-11 20:30:25 +0800

    add donater list

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,23 @@
 app.controller('MainController', ['$scope', function($scope){
-    
+    $scope.totalmoney = 514;
+    $scope.donaters = [
+        {
+            index: 1,
+            name: "Rice",
+            money: 100,
+            msg: "123456789012345678901234567890"
+        },
+        {
+            index: 2,
+            name: "Wetball",
+            money: 87,
+            msg: "def"
+        },
+        {
+            index: 3,
+            name: "Lightorz",
+            money: 78,
+            msg: "ghi"
+        }
+    ]
 }]);
