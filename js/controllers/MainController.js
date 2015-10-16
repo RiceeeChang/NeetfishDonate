059dc2427110eb1e158b4c71158d3cf38112f476
RiceeeChang <riceeechang@gmail.com>
2015-10-17 00:39:13 +0800

    complete page

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,57 @@
 app.controller('MainController', ['$scope', 'donateMsg', function($scope, donateMsg){
     $scope.donateMsg = donateMsg;
+    $scope.donating = false;
+    $scope.dname  = "";
+    $scope.money = "";
+    $scope.msg   = "";
+    $scope.modal_page = 1;
+    $scope.allpayform = "";
+
+    $scope.doDonate = function(){
+        $scope.donating = true;
+    }
+    $scope.cancelDonate = function(){
+        $scope.donating = false;
+    }
+
+    $scope.submit = function(){
+        console.log($scope.dname, $scope.money, $scope.msg);
+        
+        /*if ($scope.dname != "" && 
+            $scope.money != "" &&
+            $scope.msg   != ""){
+            
+            $.ajax({
+                type: "POST",
+                url: "http://custommaid3d2.net:8888/donate",
+                //crossDomain: true,
+                data: JSON.stringify({
+                    name:  $scope.dname,
+                    money: $scope.money,
+                    msg:   $scope.msg
+                }),
+                //contentType: "application/json",
+                //dataType: "json",
+                success: function(response){
+                    $scope.allpayform = response;
+
+                    console.log($scope.allpayform)
+
+                    $scope.dname  = "";
+                    $scope.money = "";
+                    $scope.msg   = "";
+                }
+            });
+        }*/
+        if($scope.dname != "" && $scope.money != ""){
+            document.donateForm.submit();
+            $scope.cancelDonate();
+            $scope.dname = "";
+            $scope.money = "";
+            $scope.msg   = "";
+        }
+        
+    }
+
+
 }]);
